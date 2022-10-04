5ce5695706ead4ebfe1e94eb3b13ebb912706f8e
irinasudakova <ifgoodi@yandex.ru>
2022-10-04 15:35:22 +0300

    update tests

---
+++ src/test/java/api/test/createOrder/TestSuiteCreateOrderSuccessfully.java
@@ -0,0 +1,81 @@
+package api.test.createOrder;
+
+import api.data.ingredients.BurgerFactory;
+import api.data.ingredients.Ingredients;
+import api.data.login.LoginCredentions;
+import api.data.login.LoginSuccess;
+import api.data.register.RegisterCredentials;
+import api.data.register.RegisteredUser;
+import api.data.users.AccessToken;
+import api.data.users.UsersFactory;
+import api.services.BaseUserMethod;
+import api.services.OrdersApiService;
+import io.qameta.allure.Feature;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Before;
+import org.junit.Test;
+
+import static api.conditions.Conditions.*;
+import static org.hamcrest.Matchers.*;
+
+public class TestSuiteCreateOrderSuccessfully {
+    private OrdersApiService ordersApiService;
+    private RegisterCredentials registerCredentials;
+    private RegisteredUser registeredUser;
+    private LoginSuccess loginSuccess;
+    private LoginCredentions loginCredentions;
+    private BaseUserMethod baseUserMethod;
+    private AccessToken accessToken;
+
+    @Before
+    public void setUp() {
+        ordersApiService = new OrdersApiService();
+        registeredUser = new RegisteredUser();
+        baseUserMethod = new BaseUserMethod();
+        accessToken = new AccessToken();
+        loginCredentions = new LoginCredentions();
+        loginSuccess = new LoginSuccess();
+    }
+
+    @Feature("create order")
+    @Test
+    @DisplayName("Can create order without user authorization")
+    public void testCanCreateOrderWithoutUserAuthorization() {
+        // given
+        Ingredients ingredients = ordersApiService.getIngredients().asPojo(Ingredients.class);
+
+        //expected
+        ordersApiService
+                .createOrder(BurgerFactory.getRandomBurgerWithAll(ingredients))
+                .shouldHave(statusCode(STATUS_CODE_200))
+                .shouldHave(bodyField("success", is(true)))
+                .shouldHave(bodyField("name", containsString(MESSAGE_BURGER)))
+                .shouldHave(bodyField("order.number", notNullValue()));
+    }
+
+    @Feature("create order")
+    @Test
+    @DisplayName("Can create order with user authorization")
+    public void testCanCreateOrderWithUserAuthorization() {
+        // given
+        registerCredentials = UsersFactory.getRandomUser();
+        // register and login new user
+        registeredUser = baseUserMethod.registerUserWithCurrent(registerCredentials);
+        baseUserMethod.setCurrentLoginCredentials(registerCredentials, loginCredentions);
+        loginSuccess = baseUserMethod.loginUserWithCurrent(loginCredentions,registerCredentials);
+        // get ingredients
+        Ingredients ingredients = ordersApiService.getIngredients().asPojo(Ingredients.class);
+
+        // expected
+        ordersApiService
+                .createOrder(BurgerFactory.getRandomBurgerWithAll(ingredients))
+                .shouldHave(statusCode(STATUS_CODE_200))
+                .shouldHave(bodyField("success", is(true)))
+                .shouldHave(bodyField("name", containsString(MESSAGE_BURGER)))
+                .shouldHave(bodyField("order.number", notNullValue()));
+
+        // delete user
+        accessToken.setAccessToken(loginSuccess.getAccessToken());
+        baseUserMethod.deleteUserWithCurrent(accessToken);
+    }
+}
